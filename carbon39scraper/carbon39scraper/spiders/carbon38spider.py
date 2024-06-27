644ae8e2bd202172f47459031aace2eb0787c42a
jayaram000 <jayaram.sj70@gmail.com>
2024-06-27 23:38:42 +0530

    created parse fun and adding some yields

--- carbon39scraper/carbon39scraper/spiders/carbon38spider.py
+++ carbon39scraper/carbon39scraper/spiders/carbon38spider.py
@@ -3,3 +3,10 @@ import scrapy
 class CarbonSider(scrapy.Spider):
     name = 'carbon'
     start_urls = [' https://www.carbon38.com/shop-all-activewear/tops']
+    def parse(self,response):
+        for products in response.css('div.ProductItem'):
+            yield {
+                'name': products.css('h3.ProductItem__Designer::text').get(),
+                'price': products.css('span.ProductItem__Price::text').get().replace('$',''),
+                'link': product.css('h2.ProductItem__Title a::attr(href)').get(),
+            }
