1978f0e00b11230f2efc428bf7be181768bda9d5
jayaram000 <jayaram.sj70@gmail.com>
2024-06-28 01:49:37 +0530

    created to take datas from single product

--- carbon39scraper/carbon39scraper/items.py
+++ carbon39scraper/carbon39scraper/items.py
@@ -6,7 +6,17 @@
 import scrapy
 
 
-class Carbon39ScraperItem(scrapy.Item):
+class CarbonItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    breadcrumbs = scrapy.Field()
+    image_url = scrapy.Field()
+    brand = scrapy.Field()
+    product_name = scrapy.Field()
+    price = scrapy.Field()
+    reviews = scrapy.Field()
+    colour = scrapy.Field()
+    sizes = scrapy.Field()
+    description = scrapy.Field()
+    sku = scrapy.Field()
+    product_id = scrapy.Field()
+    
--- carbon39scraper/carbon39scraper/spiders/carbon38spider.py
+++ carbon39scraper/carbon39scraper/spiders/carbon38spider.py
@@ -1,12 +1,20 @@
 import scrapy
+from carbon39scraper.items import CarbonItem
 
 class CarbonSider(scrapy.Spider):
     name = 'carbon'
     start_urls = [' https://www.carbon38.com/shop-all-activewear/tops']
+
+
     def parse(self,response):
-        for products in response.css('div.ProductItem'):
-            yield {
-                'name': products.css('h3.ProductItem__Designer::text').get(),
-                'price': products.css('span.ProductItem__Price::text').get().replace('$',''),
-                'link': products.css('h2.ProductItem__Title a::attr(href)').get(),
-            }
+        prdt_link = response.css('h2.ProductItem__Title a::attr(href)').get()
+
+        for link in prdt_link:
+            yield response.follow(link,self.parse_product)
+
+        next_page = response.css('a.Pagination__NavItem.Link.Link--primary').attrib['href']
+
+        if next_page is not None:
+            yield response.follow(next_page,self.parse)
+
+    
