4070e39c88244d214c1a4de97b07a650f75af5cd
jayaram000 <jayaram.sj70@gmail.com>
2024-06-28 16:22:58 +0530

    changed everything

--- carbon39scraper/carbon39scraper/spiders/carbon38spider.py
+++ carbon39scraper/carbon39scraper/spiders/carbon38spider.py
@@ -1,20 +1,28 @@
-import scrapy
-from carbon39scraper.items import CarbonItem
+import scrapy 
+from bs4 import BeautifulSoup
 
-class CarbonSider(scrapy.Spider):
+class CarbonSpider(scrapy.Spider):
     name = 'carbon'
     start_urls = [' https://www.carbon38.com/shop-all-activewear/tops']
 
 
     def parse(self,response):
-        prdt_link = response.css('h2.ProductItem__Title a::attr(href)').get()
+        for product in response.css('div.ProductItem'):
+            product_link = product.css('h2.ProductItem__Title a::attr(href)').get()
 
-        for link in prdt_link:
-            yield response.follow(link,self.parse_product)
+            if product_link:
+                yield response.follow(product_link, self.parse_product)
 
         next_page = response.css('a.Pagination__NavItem.Link.Link--primary').attrib['href']
-
         if next_page is not None:
-            yield response.follow(next_page,self.parse)
+            yield response.follow(next_page,callback=self.parse)
 
-    
+            
+    def parse_product(self,response):
+
+        yield{
+                'breadcrumbs': response.css('ul.breadcrumbs li a::text').getall(),
+                'name':response.css('h1.product-name::text').get().strip(),
+
+        
+        }
