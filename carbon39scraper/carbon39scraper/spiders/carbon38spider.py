1dcb875a13088adcc42d08f148082e9233fa18b5
jayaram000 <jayaram.sj70@gmail.com>
2024-07-01 02:32:32 +0530

    scrap completed

--- carbon39scraper/carbon39scraper/spiders/carbon38spider.py
+++ carbon39scraper/carbon39scraper/spiders/carbon38spider.py
@@ -43,9 +43,9 @@ class CarbonSpider(scrapy.Spider):
                 'breadcrumbs': ["home","Designers",brand,product_name],
                 'product_name': product_name,
                 'brand': brand,
-                'price': response.css('span.ProductMeta__Price::text').get().strip(),
+                'price': response.css('span.ProductMeta__Price::text').get().strip().replace('USD',''),
                 'image_url': "https:" + response.css('div.AspectRatio img::attr(src)').get().strip(),
-                'reviews': response.css('div.yotpo-sr-bottom-line-right-panel::text').get().split(" ")[2] if response.css('div.yotpo-sr-bottom-line-right-panel::text').get() else "0 Reviews",
+                'reviews': response.css('div.yotpo-sr-bottom-line-right-panel::text').get() if response.css('div.yotpo-sr-bottom-line-right-panel::text').get() else "0 Reviews",
                 'colour': response.css('span.ProductForm__SelectedValue ::text').get(),
                 'size':[size.strip() for size in response.css('li.HorizontalList__Item label::text').getall() if size.strip()],  # here i used list comprehension to take the size of the product only and the strip removes the spaces
                 "description":  response.css('div.Faq__AnswerWrapper span::text').get().strip() if response.css('div.Faq__AnswerWrapper span::text').get() else "No Description",
