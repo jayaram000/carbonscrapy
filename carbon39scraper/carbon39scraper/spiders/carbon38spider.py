9fab4a9f98155b44abcb645c7c6c262eaec2610c
jayaram000 <jayaram.sj70@gmail.com>
2024-06-30 22:54:49 +0530

    datas scrappped

--- carbon39scraper/carbon39scraper/spiders/carbon38spider.py
+++ carbon39scraper/carbon39scraper/spiders/carbon38spider.py
@@ -4,19 +4,21 @@ import re
 
 class CarbonSpider(scrapy.Spider):
     name = 'carbon'
-    start_urls = [' https://www.carbon38.com/shop-all-activewear/tops',
-                'https://carbon38.com/collections/leggings?filter.p.m.custom.available_or_waitlist=1',
-                'https://carbon38.com/collections/skirts-shorts?filter.p.m.custom.available_or_waitlist=1',
-                'https://carbon38.com/collections/sports-bras?filter.p.m.custom.available_or_waitlist=1',
-                'https://carbon38.com/collections/sweatshirts-hoodies?filter.p.m.custom.available_or_waitlist=1'
-                'https://carbon38.com/collections/bottoms?filter.p.m.custom.available_or_waitlist=1',
-                'https://carbon38.com/collections/sweaters-knits?filter.p.m.custom.available_or_waitlist=1',
+    start_urls = [' https://carbon38.com/collections/tops?filter.p.m.custom.available_or_waitlist=1',
+                # 'https://carbon38.com/collections/leggings?filter.p.m.custom.available_or_waitlist=1',
+                # 'https://carbon38.com/collections/skirts-shorts?filter.p.m.custom.available_or_waitlist=1',
+                # 'https://carbon38.com/collections/sports-bras?filter.p.m.custom.available_or_waitlist=1',
+                # 'https://carbon38.com/collections/sweatshirts-hoodies?filter.p.m.custom.available_or_waitlist=1'
+                # 'https://carbon38.com/collections/bottoms?filter.p.m.custom.available_or_waitlist=1',
+                # 'https://carbon38.com/collections/sweaters-knits?filter.p.m.custom.available_or_waitlist=1',
                 ]
 
 
     def parse(self,response):
         for product in response.css('div.ProductItem'):
             product_link = product.css('h2.ProductItem__Title a::attr(href)').get()
+            
+
 
             if product_link:
                 yield response.follow(product_link, self.parse_product)
@@ -27,27 +29,25 @@ class CarbonSpider(scrapy.Spider):
 
             
     def parse_product(self,response):
-        script_content = response.xpath('//script[contains(., "var product = [{")]/text()').get()
 
-        # Find SKU and product ID using regex
+
+        script_content = response.xpath('//script[contains(., "var product = [{")]/text()').get()  #here the sku and product_id of a particular product is extracted from the script tag with the re module
         sku_match = re.search(r"'sku'\s*:\s*\"(.*?)\"", script_content)
-        product_id_match = re.search(r"'id'\s*:\s*(\d+)", script_content)
-
         sku = sku_match.group(1) if sku_match else None
-        product_id = product_id_match.group(1) if product_id_match else None
+ 
 
         yield{
-                'breadcrumbs' : response.css('nav.breadcrumb li ::text').getall(),
+                'breadcrumbs' : response.css('li.breadcrumb ::text').getall(),
                 'product_name' : response.css('h1.ProductMeta__Title::text').get().strip(),
                 'brand' : response.css('h2.ProductMeta__Vendor a::text').get().strip(),
                 'price' : response.css('span.ProductMeta__Price::text').get().strip(),
                 'image_url' : "https:" + response.css('div.AspectRatio img::attr(src)').get().strip(),
                 'reviews' : response.css('div.yotpo-bottom-line-basic-text::text').get().split(" ")[2] if response.css('div.yotpo-bottom-line-basic-text::text').get() else "0 Reviews",
                 'colour': response.css('span.ProductForm__SelectedValue ::text').get(),
-                'size' :[size.strip() for size in response.css('li.HorizontalList__Item label::text').getall() if size.strip()],
+                'size' :[size.strip() for size in response.css('li.HorizontalList__Item label::text').getall() if size.strip()],  # here i used list comprehension to take the size of the product only and the strip removes the spaces
                 "description":  response.css('div.Faq__AnswerWrapper span::text').get().strip() if response.css('div.Faq__AnswerWrapper span::text').get() else "No Description",
                 "sku": sku,
-                "product_id":product_id,
+                "product_id":response.css('status-save-button::attr(product-id)').get()
 
         
         }
